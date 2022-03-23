2fad594427203a8531ac898e26c2a33ac73352f8
Amanda Vollmar <mandavollmar@Amandas-MBP.hsd1.co.comcast.net>
2022-03-23 12:09:50 -0600

    Add Main Class

---
+++ src/main/java/com/detroitlabs/BookTracker/AppConfiguration.java
@@ -0,0 +1,9 @@
+package com.detroitlabs.BookTracker;
+
+import org.springframework.boot.SpringApplication;
+
+public class AppConfiguration {
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfiguration.class, args);
+    }
+}
